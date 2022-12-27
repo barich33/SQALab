1ca0a30bd95db576f64b3b7f101e5e28fcd4d4d4
tagele <barich33@gmail.com>
2022-12-27 09:35:53 +0300

    Unit Test

---
+++ src/test/java/ContactManagerTest.java
@@ -0,0 +1,20 @@
+import Lab.WKU.ContactManager;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ContactManagerTest {
+
+
+}
